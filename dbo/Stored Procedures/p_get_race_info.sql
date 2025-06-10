314c7da05b2c3d7b7e29c1a784b43580934313b5
Adam Zelenka <interior.running@gmail.com>
2025-06-09 20:50:17 -0700

    Added multiple distance per race

--- dbo/Stored Procedures/p_get_race_info.sql
+++ dbo/Stored Procedures/p_get_race_info.sql
@@ -1,6 +1,10 @@
-CREATE PROCEDURE dbo.`p_get_race_info`(
-	IN Year smallint, 
-    IN UrlName varchar(50) 
+drop procedure if exists
+    dbo.p_get_race_info;
+
+create procedure
+    dbo.p_get_race_info (
+	in Year smallint, 
+    in UrlName varchar(50) 
 )
 begin
 select
@@ -13,13 +17,10 @@ select
     ra.IsRoad,
     ra.IsCrossCountry,
     ra.IsOutOfSeries,
-	ra2.Name as Name2,
-    ra2.UrlName as UrlName2,
-    d2.Distance as Distance2,
-    d2.IsMiles as IsMiles2,
     se.Name as Series,
     se.UrlName as SeriesUrlName,
-    case when exists (select 1 from dbo.results as re where re.RaceID = ra.RaceID and re.StartDelay is not null) then 1 else 0 end as StartDelay
+    case when exists (select 1 from dbo.results as re where re.RaceID = ra.RaceID and re.StartDelay is not null) then 1 else 0 end as StartDelay,
+    rd.Distances
 from
 	dbo.races as ra
 join
@@ -28,16 +29,28 @@ join
 join
 	dbo.cities as ci on
     ci.CityID = ra.CityID
-left join
-	dbo.races as ra2 on
-    ra2.Year = ra.Year and
-    ra2.Number = ra.Number and
-    ra2.IsRoad = ra.IsRoad and
-    ra2.IsCrossCountry = ra.IsCrossCountry and
-    ra2.RaceId <> ra.RaceId
-left join
-	dbo.distances as d2 on
-	d2.DistanceID = ra2.DistanceID
+join lateral 
+(
+    select 
+        json_arrayagg(
+            json_object
+            (
+                'Distance', d2.Distance,
+                'IsMiles', cast(d2.IsMiles as unsigned),
+                'UrlName', ra2.UrlName
+            )
+        ) as Distances
+    from 
+	    dbo.races as ra2    
+    join
+        dbo.distances as d2 on
+        d2.DistanceID = ra2.DistanceID
+    where 
+        ra2.Year = ra.Year and
+        ra2.Number = ra.Number and
+        ra2.IsRoad = ra.IsRoad and
+        ra2.IsCrossCountry = ra.IsCrossCountry 
+) as rd
 left join
 	dbo.series as se on
     se.SeriesID = ra.SeriesID
