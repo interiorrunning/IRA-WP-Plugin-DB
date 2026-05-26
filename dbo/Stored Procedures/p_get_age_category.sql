33850c6805fd603e70991a49991232bc22ceb986
zelenkaa <azelenka@gmail.com>
2026-05-25 21:15:48 -0700

    using athlete cache

--- dbo/Stored Procedures/p_get_age_category.sql
+++ dbo/Stored Procedures/p_get_age_category.sql
@@ -1,77 +1,76 @@
-drop procedure if exists 
-    dbo.p_get_age_category;
+DELIMITER $$
 
 create procedure dbo.p_get_age_category(
 	in Year smallint
 )
 begin
 select
-	t.Year,
+	t.`Year`,
     t.IsRoad,
     t.AgeGroup,
     t.urlname as UrlName,
-    min(t.Name) as Name,
+    min(t.`Name`) as Name,
     min(t.City) as City,
     sum(t.AdjPoints) as TotalPoints,    
-    min(if(t.Number = 1, t.AdjPoints, null)) as AdjPoints01,
-    min(if(t.Number = 2, t.AdjPoints, null)) as AdjPoints02,
-    min(if(t.Number = 3, t.AdjPoints, null)) as AdjPoints03,
-    min(if(t.Number = 4, t.AdjPoints, null)) as AdjPoints04,
-    min(if(t.Number = 5, t.AdjPoints, null)) as AdjPoints05,
-    min(if(t.Number = 6, t.AdjPoints, null)) as AdjPoints06,
-    min(if(t.Number = 7, t.AdjPoints, null)) as AdjPoints07,
-    min(if(t.Number = 8, t.AdjPoints, null)) as AdjPoints08,
-    min(if(t.Number = 9, t.AdjPoints, null)) as AdjPoints09,
-    min(if(t.Number = 10, t.AdjPoints, null)) as AdjPoints10,
-    min(if(t.Number = 11, t.AdjPoints, null)) as AdjPoints11,
-    min(if(t.Number = 12, t.AdjPoints, null)) as AdjPoints12,
-    min(if(t.Number = 13, t.AdjPoints, null)) as AdjPoints13,
-    min(if(t.Number = 1, t.Points, null)) as OriPoints01,
-    min(if(t.Number = 2, t.Points, null)) as OriPoints02,
-    min(if(t.Number = 3, t.Points, null)) as OriPoints03,
-    min(if(t.Number = 4, t.Points, null)) as OriPoints04,
-    min(if(t.Number = 5, t.Points, null)) as OriPoints05,
-    min(if(t.Number = 6, t.Points, null)) as OriPoints06,
-    min(if(t.Number = 7, t.Points, null)) as OriPoints07,
-    min(if(t.Number = 8, t.Points, null)) as OriPoints08,
-    min(if(t.Number = 9, t.Points, null)) as OriPoints09,
-    min(if(t.Number = 10, t.Points, null)) as OriPoints10,
-    min(if(t.Number = 11, t.Points, null)) as OriPoints11,
-    min(if(t.Number = 12, t.Points, null)) as OriPoints12,
-    min(if(t.Number = 13, t.Points, null)) as OriPoints13,
-    min(if(t.Number = 1, t.IsRaceDirector, null)) as IsRaceDirector01,
-    min(if(t.Number = 2, t.IsRaceDirector, null)) as IsRaceDirector02,
-    min(if(t.Number = 3, t.IsRaceDirector, null)) as IsRaceDirector03,
-    min(if(t.Number = 4, t.IsRaceDirector, null)) as IsRaceDirector04,
-    min(if(t.Number = 5, t.IsRaceDirector, null)) as IsRaceDirector05,
-    min(if(t.Number = 6, t.IsRaceDirector, null)) as IsRaceDirector06,
-    min(if(t.Number = 7, t.IsRaceDirector, null)) as IsRaceDirector07,
-    min(if(t.Number = 8, t.IsRaceDirector, null)) as IsRaceDirector08,
-    min(if(t.Number = 9, t.IsRaceDirector, null)) as IsRaceDirector09,
-    min(if(t.Number = 10, t.IsRaceDirector, null)) as IsRaceDirector10,
-    min(if(t.Number = 11, t.IsRaceDirector, null)) as IsRaceDirector11,
-    min(if(t.Number = 12, t.IsRaceDirector, null)) as IsRaceDirector12,
-    min(if(t.Number = 13, t.IsRaceDirector, null)) as IsRaceDirector13
+    min(if(t.`Number` = 1, t.AdjPoints, null)) as AdjPoints01,
+    min(if(t.`Number` = 2, t.AdjPoints, null)) as AdjPoints02,
+    min(if(t.`Number` = 3, t.AdjPoints, null)) as AdjPoints03,
+    min(if(t.`Number` = 4, t.AdjPoints, null)) as AdjPoints04,
+    min(if(t.`Number` = 5, t.AdjPoints, null)) as AdjPoints05,
+    min(if(t.`Number` = 6, t.AdjPoints, null)) as AdjPoints06,
+    min(if(t.`Number` = 7, t.AdjPoints, null)) as AdjPoints07,
+    min(if(t.`Number` = 8, t.AdjPoints, null)) as AdjPoints08,
+    min(if(t.`Number` = 9, t.AdjPoints, null)) as AdjPoints09,
+    min(if(t.`Number` = 10, t.AdjPoints, null)) as AdjPoints10,
+    min(if(t.`Number` = 11, t.AdjPoints, null)) as AdjPoints11,
+    min(if(t.`Number` = 12, t.AdjPoints, null)) as AdjPoints12,
+    min(if(t.`Number` = 13, t.AdjPoints, null)) as AdjPoints13,
+    min(if(t.`Number` = 1, t.Points, null)) as OriPoints01,
+    min(if(t.`Number` = 2, t.Points, null)) as OriPoints02,
+    min(if(t.`Number` = 3, t.Points, null)) as OriPoints03,
+    min(if(t.`Number` = 4, t.Points, null)) as OriPoints04,
+    min(if(t.`Number` = 5, t.Points, null)) as OriPoints05,
+    min(if(t.`Number` = 6, t.Points, null)) as OriPoints06,
+    min(if(t.`Number` = 7, t.Points, null)) as OriPoints07,
+    min(if(t.`Number` = 8, t.Points, null)) as OriPoints08,
+    min(if(t.`Number` = 9, t.Points, null)) as OriPoints09,
+    min(if(t.`Number` = 10, t.Points, null)) as OriPoints10,
+    min(if(t.`Number` = 11, t.Points, null)) as OriPoints11,
+    min(if(t.`Number` = 12, t.Points, null)) as OriPoints12,
+    min(if(t.`Number` = 13, t.Points, null)) as OriPoints13,
+    min(if(t.`Number` = 1, t.IsRaceDirector, null)) as IsRaceDirector01,
+    min(if(t.`Number` = 2, t.IsRaceDirector, null)) as IsRaceDirector02,
+    min(if(t.`Number` = 3, t.IsRaceDirector, null)) as IsRaceDirector03,
+    min(if(t.`Number` = 4, t.IsRaceDirector, null)) as IsRaceDirector04,
+    min(if(t.`Number` = 5, t.IsRaceDirector, null)) as IsRaceDirector05,
+    min(if(t.`Number` = 6, t.IsRaceDirector, null)) as IsRaceDirector06,
+    min(if(t.`Number` = 7, t.IsRaceDirector, null)) as IsRaceDirector07,
+    min(if(t.`Number` = 8, t.IsRaceDirector, null)) as IsRaceDirector08,
+    min(if(t.`Number` = 9, t.IsRaceDirector, null)) as IsRaceDirector09,
+    min(if(t.`Number` = 10, t.IsRaceDirector, null)) as IsRaceDirector10,
+    min(if(t.`Number` = 11, t.IsRaceDirector, null)) as IsRaceDirector11,
+    min(if(t.`Number` = 12, t.IsRaceDirector, null)) as IsRaceDirector12,
+    min(if(t.`Number` = 13, t.IsRaceDirector, null)) as IsRaceDirector13
 from
 (
     select 
-        t.Year,
+        t.`Year`,
         t.IsRoad,
-        t.Number,
+        t.`Number`,
         t.AgeGroup,    
         t.urlname,
-        t.Name,
+        t.`Name`,
         t.City,
-        coalesce(t.Points, min(t.Points) over (partition by t.Year, t.IsRoad, t.AgeGroup, t.urlname)) as Points,    
-        row_number() over (partition by t.Year, t.IsRoad, t.AgeGroup, t.urlname order by if(t.Points is not null, 0, 1), t.Points, t.Number) as rn,    
+        coalesce(t.Points, min(t.Points) over (partition by t.`Year`, t.IsRoad, t.AgeGroup, t.urlname)) as Points,    
+        row_number() over (partition by t.`Year`, t.IsRoad, t.AgeGroup, t.urlname order by if(t.Points is not null, 0, 1), t.Points, t.`Number`) as rn,    
         if
         (
             er.MaxRaces >=
-            row_number() over (partition by t.Year, t.IsRoad, t.AgeGroup, t.urlname order by if(t.Points is not null, 0, 1), t.Points desc, t.Number),
+            row_number() over (partition by t.`Year`, t.IsRoad, t.AgeGroup, t.urlname order by if(t.Points is not null, 0, 1), t.Points desc, t.`Number`),
             coalesce
             (
                 t.Points,
-                min(t.Points) over (partition by t.Year, t.IsRoad, t.AgeGroup, t.urlname)
+                min(t.Points) over (partition by t.`Year`, t.IsRoad, t.AgeGroup, t.urlname)
             ),
             null
         ) as AdjPoints,
@@ -79,12 +78,12 @@ from
     from
     (
         select
-            t.Year,
+            t.`Year`,
             t.IsRoad,
-            t.Number,    
+            t.`Number`,    
             t.AgeGroup,
             t.urlname,
-            t.Name,
+            t.`Name`,
             t.City,
             case
                 when
@@ -106,34 +105,32 @@ from
         from
         (
             select
-                ra.Year,
+                ra.`Year`,
                 ra.IsRoad,
-                ra.Number,    
-                di.AgeGroup,
+                ra.`Number`,    
+                cai.AgeGroup,
                 li.urlname,
-                na.Name,
-                ci.Name as City,
-                rank() over (partition by ra.RaceID, di.AgeGroup order by re.Time) as Place,
+                cai.`Name`,
+                cai.City,
+                rank() over (partition by ra.RaceID, cai.AgeGroup order by re.`Time`) as Place,
                 null as IsRaceDirector
             from
                 dbo.races as ra
             join
                 dbo.results as re on
-                ra.RaceID = re.RaceID
-            join
-                dbo.names as na on
-                na.NameID = re.NameID
-            join
-                dbo.cities as ci on
-                ci.CityID = re.CityID
+                ra.RaceID = re.RaceID           
             join
                 dbo.links as li on
                 li.LinkID = re.LinkID
+            join 
+                dbo.cache_athlete_info as cai on 
+                cai.`Year` = ra.`Year` and 
+                cai.LinkID = re.LinkID
             join
                 dbo.divisions as di on
                 di.DivisionID = re.DivisionID
             where
-                ra.Year = Year and    
+                ra.`Year` = Year and    
                 ra.IsOutOfSeries = 0 and
                 di.IsWheeler = 0 and 
                 (
@@ -145,12 +142,12 @@ from
         union all
             
         select
-            ra.Year,
+            ra.`Year`,
             ra.IsRoad,
-            ra.Number,    
+            ra.`Number`,    
             di.AgeGroup,
             li.urlname,
-            cai.Name,
+            cai.`Name`,
             cai.City,
             null as Place,
             rd.IsRaceDirector
@@ -167,10 +164,10 @@ from
             li.LinkID = rd.LinkID
         join 
             dbo.cache_athlete_info as cai on 
-            cai.Year = ra.Year and 
+            cai.`Year` = ra.`Year` and 
             cai.LinkID = rd.LinkID
         where
-            ra.Year = Year and 
+            ra.`Year` = Year and 
             (
                 di.IsFemale = 1 or 
                 di.IsMale = 1
@@ -178,11 +175,11 @@ from
     ) as t
     join
         dbo.eligible_races as er on
-        er.Year = t.Year and
+        er.`Year` = t.`Year` and
         er.IsRoad = t.IsRoad
 ) as t
 group by
-	t.Year,
+	t.`Year`,
     t.IsRoad,
     t.AgeGroup,
     t.urlname
@@ -192,5 +189,6 @@ order by
     TotalPoints desc,
     urlname;
 
-    
-end
+end$$
+
+DELIMITER ;
--- dbo/Stored Procedures/p_get_age_grade.sql
+++ dbo/Stored Procedures/p_get_age_grade.sql
@@ -1,27 +1,30 @@
-CREATE PROCEDURE dbo.`p_get_age_grade`(
+DELIMITER $$
+
+create procedure
+    dbo.p_get_age_grade(
 	IN Year smallint
 )
 begin
 select
-	t.Year as y,
+	t.`Year` as y,
     t.IsRoad as ir,
-    t.Number as n,
+    t.`Number` as n,
 	t.urlname as ur,
-	t.Name as na,
+	t.`Name` as na,
 	t.City as ci,
 	t.AgeGroup as di,
 	t.ag as ag,
 	t.pnts as p1,
-	if(if(t.Year >= 2025, t.rnk2, t.rnk) <= er.MaxRaces, t.pnts, null) as p2
+	if(if(t.`Year` >= 2025, t.rnk2, t.rnk) <= er.MaxRaces, t.pnts, null) as p2
 from
 (
     select
-        t.Year,
+        t.`Year`,
         t.IsRoad,
-        t.Number,
+        t.`Number`,
         t.LinkID,
         t.urlname,
-        if(t.rn = 1, t.Name, null) as Name,
+        if(t.rn = 1, t.`Name`, null) as Name,
         if(t.rn = 1, t.City, null) as City,
         t.AgeGroup,
         t.ag,
@@ -44,55 +47,55 @@ from
             else
                 194 - t.pl
             end as pnts,
-        row_number() over (partition by t.Year, t.IsRoad, t.LinkID order by t.pl, t.Number) as rnk,
-        row_number() over (partition by t.Year, t.IsRoad, t.LinkID order by t.ag desc, t.Number) as rnk2
+        row_number() over (partition by t.`Year`, t.IsRoad, t.LinkID order by t.pl, t.Number) as rnk,
+        row_number() over (partition by t.`Year`, t.IsRoad, t.LinkID order by t.ag desc, t.Number) as rnk2
     from
     (
         select
-            ra.Year,
+            ra.`Year`,
             ra.IsRoad,
-            ra.Number,
+            ra.`Number`,
             li.LinkID,
             li.urlname,
-            na.Name,
-            ci.Name as City,
-            di.AgeGroup,
+            cai.`Name`,
+            cai.City,
+            cai.AgeGroup,
             re2.AgeGrade as ag,
-            rank() over (partition by ra.Year, ra.IsRoad, ra.Number, di.IsMale order by re2.AgeGrade desc) as pl,
-            row_number() over (partition by ra.Year, ra.IsRoad, li.LinkID order by ra.Number desc) as rn
+            rank() over (partition by ra.`Year`, ra.IsRoad, ra.Number, di.IsMale order by re2.AgeGrade desc) as pl,
+            row_number() over (partition by ra.`Year`, ra.IsRoad, li.LinkID order by ra.Number desc) as rn
         from
             dbo.races as ra
         join
             dbo.results as re on
             ra.RaceID = re.RaceID
         join
-            dbo.cache_results as re2 on
-            re2.ResultID = re.ResultID
-        join
-            dbo.divisions as di on
-            di.DivisionID = re.DivisionID
-        join
             dbo.links as li on
             li.LinkID = re.LinkID
+        join 
+            dbo.divisions as di on 
+            di.DivisionID = re.DivisionID
         join
-            dbo.names as na on
-            na.NameID = re.NameID
-        join
-            dbo.cities as ci on
-            ci.CityID = ra.CityID
+            dbo.cache_results as re2 on
+            re2.ResultID = re.ResultID        
+        join 
+            dbo.cache_athlete_info as cai on 
+            cai.LinkID = li.LinkID and 
+            cai.`Year` = ra.`Year`
         where
-            ra.Year = Year and
+            ra.`Year` = Year and
             ra.IsOutOfSeries = 0 and
             di.IsWheeler = 0
     ) as t
 ) as t
 join
 	dbo.eligible_races as er on
-    er.Year = t.Year and
+    er.`Year` = t.`Year` and
     er.IsRoad = t.IsRoad
 order by
-	t.Year,
+	t.`Year`,
     t.IsRoad,
     t.LinkID,
-    t.Number;
-end
+    t.`Number`;
+end$$
+
+DELIMITER ;
